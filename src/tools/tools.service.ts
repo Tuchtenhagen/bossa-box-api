1c08677a9d94795d53e983ea28bb27338a4f4715
Richard Tuchtenhagen <william.10.xavier@hotmail.com>
2021-08-21 15:36:30 -0300

    fix .env and other things

--- src/tools/tools.service.ts
+++ src/tools/tools.service.ts
@@ -11,29 +11,6 @@ export class ToolsService {
     @InjectRepository(Tool)
     private toolsRepository: Repository<Tool>,
   ) {}
-  tools: UpdateToolDto[] = [
-    {
-      id: 1,
-      title: 'title one',
-      description: 'description one',
-      link: 'link one',
-      tags: ['tag one'],
-    },
-    {
-      id: 2,
-      title: 'title two',
-      description: 'description two',
-      link: 'link two',
-      tags: ['tag two'],
-    },
-    {
-      id: 3,
-      title: 'title three',
-      description: 'description three',
-      link: 'link three',
-      tags: ['tag three'],
-    },
-  ];
 
   async create(createToolDto: CreateToolDto) {
     const tool = await this.toolsRepository.save(createToolDto);
